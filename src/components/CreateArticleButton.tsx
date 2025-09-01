a9d4e6486a1ae7ecb9ec1742f531bf899b1942da
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-09-01 15:57:56 +0200

    chore :backoffice table display ok

--- src/components/CreateArticleButton.tsx
+++ src/components/CreateArticleButton.tsx
@@ -4,7 +4,7 @@ import CreateArticleModal from "./CreateArticleModal.tsx";
 export default function CreateArticleButton() {
     const role = typeof window !== "undefined" ? localStorage.getItem("role_id") : null;
 
-    if (role !== "2") return null; // affichage conditionnel
+    if (role !== "3" && role !== "1") return null; // affichage conditionnel
 
     return (
         <CreateArticleModal onArticleCreated={() => window.dispatchEvent(new Event("article-created"))} />
--- src/components/CreateEventButton.tsx
+++ src/components/CreateEventButton.tsx
@@ -4,7 +4,7 @@ import CreateEventModal from "./CreateEventModal.tsx";
 export default function CreateEventButton() {
     const role = typeof window !== "undefined" ? localStorage.getItem("role_id") : null;
 
-    if (role !== "2") return null; // affichage conditionnel
+    if (role !== "3" && role !== "1") return null; // affichage conditionnel
 
     return (
         <CreateEventModal onEventCreated={() => window.dispatchEvent(new Event("event-created"))} />
